ce17bcf7ef748020ee4b6fb3a81d2082772783dc
YibingZhu <zhu.yib@northeastern.edu>
2021-12-09 17:41:30 -0500

    missing files after merge conflicts readded

---
+++ FoodBankDemo/src/Business/Menu/MenuDirectory.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Menu;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author yibin
+ */
+public class MenuDirectory {
+
+    private ArrayList<Menu> menuDirectory;
+
+    public MenuDirectory() {
+        menuDirectory = new ArrayList();
+    }
+
+    public ArrayList<Menu> getMenuDirectory() {
+        return menuDirectory;
+    }
+
+    public void setMenuDirectory(ArrayList<Menu> menuDirectory) {
+        this.menuDirectory = menuDirectory;
+    }
+
+    public Menu newItem(String itemName, int qty, String unit, String id, String res) {
+        Menu menu = new Menu(itemName, qty, unit, id, res);
+        menuDirectory.add(menu);
+        return menu;
+    }
+
+    public void removeMenu(Menu menu) {
+        menuDirectory.remove(menu);
+    }
+}
