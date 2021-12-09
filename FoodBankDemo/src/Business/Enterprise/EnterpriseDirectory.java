152459ad1cab69183d8620a5cb33c03baa8b47eb
wangjicneu <91155513+wangjicneu@users.noreply.github.com>
2021-12-09 14:26:12 -0500

    update EnterpriseDirectory

--- FoodBankDemo/src/Business/Enterprise/EnterpriseDirectory.java
+++ FoodBankDemo/src/Business/Enterprise/EnterpriseDirectory.java
@@ -13,8 +13,8 @@ import java.util.ArrayList;
  * @author MyPC1
  */
 public class EnterpriseDirectory {
+
     private ArrayList<Enterprise> enterpriseList;
-   
 
     public ArrayList<Enterprise> getEnterpriseList() {
         return enterpriseList;
@@ -23,24 +23,27 @@ public class EnterpriseDirectory {
     public void setEnterpriseList(ArrayList<Enterprise> enterpriseList) {
         this.enterpriseList = enterpriseList;
     }
-    
-    public EnterpriseDirectory(){
-        enterpriseList=new ArrayList<Enterprise>();
+
+    public EnterpriseDirectory() {
+        enterpriseList = new ArrayList<Enterprise>();
     }
-    
+
     //Create enterprise
-    public Enterprise createAndAddEnterprise(String name,Enterprise.EnterpriseType type){
-        Enterprise enterprise=null;
-        if(type==Enterprise.EnterpriseType.Hospital){
-            enterprise=new HospitalEnterprise(name);
+    public Enterprise createAndAddEnterprise(String name, Enterprise.EnterpriseType type) {
+        Enterprise enterprise = null;
+        if (type == Enterprise.EnterpriseType.Hospital) {
+            enterprise = new HospitalEnterprise(name);
             enterpriseList.add(enterprise);
-        }else if (type == Enterprise.EnterpriseType.FoodBank){
-              enterprise=new FoodBankEnterprise(name);
-              enterpriseList.add(enterprise);
-        }else if (type == Enterprise.EnterpriseType.Funding){
-              enterprise=new FundingEnterprise(name);
-              enterpriseList.add(enterprise);
+        } else if (type == Enterprise.EnterpriseType.FoodBank) {
+            enterprise = new FoodBankEnterprise(name);
+            enterpriseList.add(enterprise);
+        } else if (type == Enterprise.EnterpriseType.Funding) {
+            enterprise = new FundingEnterprise(name);
+            enterpriseList.add(enterprise);
+        } else if (type == Enterprise.EnterpriseType.Volunteer) {
+            enterprise = new VolunteerEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+        return enterprise;
     }
-       return enterprise; 
-}
 }
