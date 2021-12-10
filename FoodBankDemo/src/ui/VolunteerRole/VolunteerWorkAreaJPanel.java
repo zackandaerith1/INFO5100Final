029dd9fa0e77b3bd453e43a696e8c1b1cce2b12f
wangjicneu <91155513+wangjicneu@users.noreply.github.com>
2021-12-09 20:11:25 -0500

    VolunteerJPanel update

--- FoodBankDemo/src/ui/VolunteerRole/VolunteerWorkAreaJPanel.java
+++ FoodBankDemo/src/ui/VolunteerRole/VolunteerWorkAreaJPanel.java
@@ -40,14 +40,13 @@ public class VolunteerWorkAreaJPanel extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 600, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 600, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
