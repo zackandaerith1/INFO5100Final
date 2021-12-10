e4310e78751c2f00c254b0ecc2365125e3bf985d
zackandaerith1 <fang.zheng3@northeastern.edu>
2021-12-10 12:02:01 -0500

    structure update

--- FoodBankDemo/src/ui/ShelterAdminRole/CreateCoordinatorJPanel.java
+++ FoodBankDemo/src/ui/ShelterAdminRole/CreateCoordinatorJPanel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ui.CoordinatorRole;
+package ui.ShelterAdminRole;
 
 import Business.Coordinator.CoordinatorDirectory;
 import Business.EcoSystem;
--- FoodBankDemo/src/ui/ShelterAdminRole/ManageCoordinatorJPanel.java
+++ FoodBankDemo/src/ui/ShelterAdminRole/ManageCoordinatorJPanel.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ui.CoordinatorRole;
+package ui.ShelterAdminRole;
 
 import ui.CoordinatorRole.CreateCoordinatorJPanel;
 import Business.Coordinator.Coordinator;
