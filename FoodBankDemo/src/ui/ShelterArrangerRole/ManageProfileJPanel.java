50ae20eb8330e76de50839768bd8fc51b40caf55
YibingZhu <zhu.yib@northeastern.edu>
2021-12-10 23:53:42 -0500

    ManageProfileJPanel

--- FoodBankDemo/src/ui/ShelterArrangerRole/ManageProfileJPanel.java
+++ FoodBankDemo/src/ui/ShelterArrangerRole/ManageProfileJPanel.java
@@ -5,9 +5,7 @@
  */
 package ui.ShelterArrangerRole;
 
-import Business.EcoSystem;
 import Business.Enterprise.ShelterEnteriprise;
-import Business.Organization.Shelter.ArrangementOrganization;
 import Business.Profile.Profile;
 import Business.Profile.ProfileDirectory;
 import Business.Shelter.Shelter;
@@ -15,8 +13,6 @@ import Business.Shelter.ShelterDirectory;
 import Business.UserAccount.UserAccount;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
-import ui.ShelterArrangerRole.ModifyProfileJPanel;
-
 import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.Image;
@@ -55,7 +51,7 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
     private ProfileDirectory profileDirectory;
     private Shelter shelter;
 
-    public ManageProfileJPanel(JPanel userProcessContainer, UserAccount account,  ShelterEnteriprise enterprise) {
+    public ManageProfileJPanel(JPanel userProcessContainer, UserAccount account, ShelterEnteriprise enterprise) {
         initComponents();
         this.userProcessContainer = userProcessContainer;
         this.account = account;
@@ -130,15 +126,16 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
         radioMale = new javax.swing.JRadioButton();
         radioFemale = new javax.swing.JRadioButton();
         radioOther = new javax.swing.JRadioButton();
-        btnUpload = new javax.swing.JButton();
 
-        setBackground(new java.awt.Color(54, 33, 89));
+        setBackground(new java.awt.Color(233, 230, 225));
+        setForeground(new java.awt.Color(43, 71, 92));
 
         jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
         jScrollPane1.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         jScrollPane1.setAutoscrolls(true);
 
-        tblProfile.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
+        tblProfile.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        tblProfile.setForeground(new java.awt.Color(43, 71, 92));
         tblProfile.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null, null, null, null, null, null, null},
@@ -176,12 +173,12 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
             tblProfile.getColumnModel().getColumn(0).setMaxWidth(0);
         }
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setFont(new java.awt.Font("Yu Gothic UI", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(43, 71, 92));
         jLabel1.setText("Register Person");
 
-        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(43, 71, 92));
         jLabel3.setText("First Name:");
 
         txtFirstName.addActionListener(new java.awt.event.ActionListener() {
@@ -190,9 +187,9 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
             }
         });
 
-        btnAdd.setBackground(new java.awt.Color(122, 72, 221));
-        btnAdd.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        btnAdd.setForeground(new java.awt.Color(255, 255, 255));
+        btnAdd.setBackground(new java.awt.Color(94, 138, 117));
+        btnAdd.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        btnAdd.setForeground(new java.awt.Color(43, 71, 92));
         btnAdd.setText("Add Record");
         btnAdd.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -200,9 +197,9 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
             }
         });
 
-        btnBack.setBackground(new java.awt.Color(122, 72, 221));
-        btnBack.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        btnBack.setForeground(new java.awt.Color(255, 255, 255));
+        btnBack.setBackground(new java.awt.Color(94, 138, 117));
+        btnBack.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        btnBack.setForeground(new java.awt.Color(43, 71, 92));
         btnBack.setText("<<Back");
         btnBack.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -210,9 +207,9 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
             }
         });
 
-        delete.setBackground(new java.awt.Color(122, 72, 221));
-        delete.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        delete.setForeground(new java.awt.Color(255, 255, 255));
+        delete.setBackground(new java.awt.Color(94, 138, 117));
+        delete.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        delete.setForeground(new java.awt.Color(43, 71, 92));
         delete.setText("Delete Record");
         delete.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -220,9 +217,9 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
             }
         });
 
-        viewBtn.setBackground(new java.awt.Color(122, 72, 221));
-        viewBtn.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
-        viewBtn.setForeground(new java.awt.Color(255, 255, 255));
+        viewBtn.setBackground(new java.awt.Color(94, 138, 117));
+        viewBtn.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        viewBtn.setForeground(new java.awt.Color(43, 71, 92));
         viewBtn.setText("View/Update Record");
         viewBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -230,8 +227,8 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
             }
         });
 
-        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(43, 71, 92));
         jLabel5.setText("Last Name:");
 
         txtLastName.addActionListener(new java.awt.event.ActionListener() {
@@ -240,8 +237,8 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
             }
         });
 
-        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(43, 71, 92));
         jLabel6.setText("Phone Number:");
 
         txtPhone.addActionListener(new java.awt.event.ActionListener() {
@@ -250,16 +247,16 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
             }
         });
 
-        jLabel7.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(43, 71, 92));
         jLabel7.setText("Birth Day:");
 
-        jLabel8.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(43, 71, 92));
         jLabel8.setText("Gender:");
 
-        jLabel9.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(43, 71, 92));
         jLabel9.setText("Home Address:");
 
         txtAddress.addActionListener(new java.awt.event.ActionListener() {
@@ -268,8 +265,8 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
             }
         });
 
-        jLabel10.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel10.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel10.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(43, 71, 92));
         jLabel10.setText("Email Address:");
 
         txtEmail.addActionListener(new java.awt.event.ActionListener() {
@@ -278,8 +275,8 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
             }
         });
 
-        jLabel11.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel11.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel11.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        jLabel11.setForeground(new java.awt.Color(43, 71, 92));
         jLabel11.setText("Comment:");
 
         txtComment.setColumns(20);
@@ -288,17 +285,17 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
 
         jSeparator1.setOrientation(javax.swing.SwingConstants.VERTICAL);
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setFont(new java.awt.Font("Yu Gothic UI", 1, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(43, 71, 92));
         jLabel2.setText("Person List");
 
-        jLabel12.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel12.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel12.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        jLabel12.setForeground(new java.awt.Color(43, 71, 92));
         jLabel12.setText("Upload Picture:");
 
-        btnChoose.setBackground(new java.awt.Color(122, 72, 221));
-        btnChoose.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        btnChoose.setForeground(new java.awt.Color(255, 255, 255));
+        btnChoose.setBackground(new java.awt.Color(94, 138, 117));
+        btnChoose.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        btnChoose.setForeground(new java.awt.Color(43, 71, 92));
         btnChoose.setText("Take Picture");
         btnChoose.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -312,27 +309,23 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
         txtFilePath.setForeground(new java.awt.Color(54, 33, 89));
         txtFilePath.setToolTipText("");
 
+        radioMale.setBackground(new java.awt.Color(94, 138, 117));
         buttonGroup1.add(radioMale);
-        radioMale.setForeground(new java.awt.Color(255, 255, 255));
+        radioMale.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        radioMale.setForeground(new java.awt.Color(43, 71, 92));
         radioMale.setText("Male");
 
+        radioFemale.setBackground(new java.awt.Color(94, 138, 117));
         buttonGroup1.add(radioFemale);
-        radioFemale.setForeground(new java.awt.Color(255, 255, 255));
+        radioFemale.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        radioFemale.setForeground(new java.awt.Color(43, 71, 92));
         radioFemale.setText("Female");
 
+        radioOther.setBackground(new java.awt.Color(94, 138, 117));
         buttonGroup1.add(radioOther);
-        radioOther.setForeground(new java.awt.Color(255, 255, 255));
+        radioOther.setFont(new java.awt.Font("Yu Gothic UI", 1, 14)); // NOI18N
+        radioOther.setForeground(new java.awt.Color(43, 71, 92));
         radioOther.setText("Other");
-
-        btnUpload.setBackground(new java.awt.Color(122, 72, 221));
-        btnUpload.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        btnUpload.setForeground(new java.awt.Color(255, 255, 255));
-        btnUpload.setText("Upload Picture");
-        btnUpload.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnUploadActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -372,14 +365,12 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel12)
                                 .addGap(18, 18, 18)
-                                .addComponent(btnChoose)
-                                .addGap(18, 18, 18)
-                                .addComponent(btnUpload))
+                                .addComponent(btnChoose))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(253, 253, 253)
                                 .addComponent(txtFilePath, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addComponent(photoComponent, javax.swing.GroupLayout.PREFERRED_SIZE, 320, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(27, 27, 27)
+                        .addGap(37, 37, 37)
                         .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -411,7 +402,7 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
                                 .addComponent(viewBtn)
                                 .addGap(93, 93, 93)
                                 .addComponent(delete, javax.swing.GroupLayout.PREFERRED_SIZE, 141, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGap(0, 134, Short.MAX_VALUE)))
+                        .addGap(0, 146, Short.MAX_VALUE)))
                 .addContainerGap())
         );
 
@@ -473,8 +464,7 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
                 .addGap(19, 19, 19)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel12)
-                    .addComponent(btnChoose)
-                    .addComponent(btnUpload))
+                    .addComponent(btnChoose))
                 .addGap(18, 18, 18)
                 .addComponent(photoComponent, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -654,15 +644,10 @@ public class ManageProfileJPanel extends javax.swing.JPanel {
 
     }//GEN-LAST:event_btnChooseActionPerformed
 
-    private void btnUploadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUploadActionPerformed
-
-    }//GEN-LAST:event_btnUploadActionPerformed
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAdd;
     private javax.swing.JButton btnBack;
     private javax.swing.JButton btnChoose;
-    private javax.swing.JButton btnUpload;
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JButton delete;
     private com.toedter.calendar.JDateChooser jDateChooser;
